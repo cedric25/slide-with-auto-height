b12bfca782f84b293818888d06cc4c1ba04c62d6
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2023-03-06 15:49:53 +0100

    Start slide transition

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,100 @@
 <template>
-  <div class="w-[250px] mt-12 mx-auto">
-    <div class="relative border border-red-200 h-[300px] rounded-lg">
-      <div class="absolute border border-blue-200 h-20 rounded-lg p-1">
-        Groups
-      </div>
-      <div
-        class="absolute border border-green-200 h-12 left-[100px] p-1 rounded-lg"
-      >
-        Categories
-      </div>
+  <div class="w-[350px] mt-12 mx-auto">
+    <div class="parent bg-white h-[506px] rounded-lg">
+      <Transition name="groups">
+        <div v-if="showGroups" class="content-groups w-full">
+          <div class="flex flex-col rounded-lg px-px py-px">
+            <OneCategory
+              icon="💰"
+              label="Income & transfers"
+              @select="toggle"
+              class="rounded-t-lg"
+            />
+            <OneCategory icon="⚙️" label="Running costs" @select="toggle" />
+            <OneCategory icon="✈️" label="Meals & travel" @select="toggle" />
+            <OneCategory icon="🚗" label="Vehicle" @select="toggle" />
+            <OneCategory icon="🏦" label="Financial costs" @select="toggle" />
+            <OneCategory icon="⚒️" label="Cost of goods sold" @select="toggle" />
+            <OneCategory icon="🧵" label="Miscellaneous" @select="toggle" />
+            <OneCategory icon="🏡" label="Home office" @select="toggle" />
+            <OneCategory
+              icon="✌️"
+              label="Personal"
+              @select="toggle"
+              class="!border-b-0 rounded-b-lg"
+            />
+          </div>
+        </div>
+      </Transition>
+
+      <Transition name="categories">
+        <div
+          v-if="showCategories"
+          class="content-categories border border-green-200 h-12 w-full p-1 rounded-lg"
+        >
+          <button class="py-2 px-3 w-full text-left" @click="toggle">← Back</button>
+          <div>Categories</div>
+        </div>
+      </Transition>
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import OneCategory from './components/OneCategory.vue'
+
+const showGroups = ref(true)
+const showCategories = ref(false)
+
+function toggle() {
+  showGroups.value = !showGroups.value
+  showCategories.value = !showCategories.value
+}
+</script>
+
+<style>
+.parent {
+  position: relative;
+  overflow-y: hidden;
+}
+
+.content-groups,
+.content-categories {
+  position: absolute;
+  /* v2 */
+  /* position: absolute; // Set programmatically after doing JS height transition */
+  top: 0;
+  width: 100%;
+  height: auto;
+}
+.content-groups {
+  left: 0;
+  z-index: 0;
+}
+.content-categories {
+  left: 100%;
+  transform: translate3d(-100%, 0, 0);
+  z-index: 1;
+}
+
+.groups-enter-active,
+.groups-leave-active,
+.categories-enter-active,
+.categories-leave-active {
+  transition: transform 0.3s ease-in-out;
+  /*transition: transform 1s ease-in-out;*/
+}
+.groups-enter-from,
+.groups-leave-to,
+.categories-leave-from,
+.categories-enter-to {
+  transform: translate3d(-100%, 0, 0);
+}
+.groups-leave-from,
+.groups-enter-to,
+.categories-enter-from,
+.categories-leave-to {
+  transform: translate3d(0, 0, 0);
+}
+</style>
---
+++ src/components/OneCategory.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="one-group h-14 w-full text-left p-1 border-b px-4" @click="$emit('select')">
+    <span class="mr-2">{{ icon }}</span>
+    {{ label }}
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  icon: string
+  label: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'select'): void
+}>()
+</script>
+
+<style>
+.one-group:hover {
+  transition: background-color 0.2s ease-in-out;
+  background-color: #f5f6f9;
+}
+</style>
